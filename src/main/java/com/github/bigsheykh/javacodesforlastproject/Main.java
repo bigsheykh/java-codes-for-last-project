2c4b096096abce09cfa31ccd47e7e6137d778a7d
bigsheykh <amir.sheykhshirmast@gmail.com>
2023-10-08 16:11:33 +0330

    make directory layout maven standard.

---
+++ src/main/java/com/github/bigsheykh/javacodesforlastproject/Main.java
@@ -0,0 +1,8 @@
+package com.github.bigsheykh.javacodesforlastproject;
+import java.util.Objects;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
